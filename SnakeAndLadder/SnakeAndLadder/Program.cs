c4c2d197526770af5fd0776aedd6c89b8dbfd50b
Ashu <ashusarnaik11@gmail.com>
2021-11-24 23:57:53 +0530

    win position

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -7,7 +7,20 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
            Random snakeAndLadder1= new Random();
-            snakeAndLadder1.Next();
+            int player1 = snakeAndLadder1.Play(0);
+            int player2 = snakeAndLadder1.Play(0);
+            if (player1.CompareTo(player2) == 0)
+            {
+                Console.WriteLine(" Player1 & Player2 at same position " + player1 + ",It is Tie ");
+            }
+            if (player1.CompareTo(player2) > 0)
+            {
+                Console.WriteLine("Player1 win " + player1);
+            }
+            if (player1.CompareTo(player2) < 0)
+            {
+                Console.WriteLine("Player2 win " + player2);
+            }
             Console.ReadKey();
         }
     }
